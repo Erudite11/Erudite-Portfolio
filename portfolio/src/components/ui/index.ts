1135b89ed03b7f691c109b4ac41ebf8f02362cb8
Erudite <oshiyokuolaoluwa@gmail.com>
2026-01-13 23:15:19 +0100

    fix: use local avatar image instead of GitHub URL

--- portfolio/src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from "./Button";
-export { Container } from "./Container";
-export { SectionHeading } from "./SectionHeading";
-export { Card } from "./Card";
-export { Reveal } from "./Reveal";
